e2322cfc5e2d8cd9105cceb4f0d5a5c3b84b1a4d
ylws <474569696@qq.com>
2017-07-14 15:19:33 +0800

    vue install

--- calendar/index.js
+++ calendar/index.js
@@ -1,6 +1,6 @@
 import {CFCMCalendar} from './src/Plugins_es6_calendar.babel.min.js'
 export default {
- 	install: function(Vue, name = '$Calendar') {
-	 Object.defineProperty(Vue.prototype, name, { value: CFCMCalendar });
+ 	install: function(Vue) {
+	 Object.defineProperty(Vue.prototype, '$Calendar', { value: CFCMCalendar });
   }
 }
